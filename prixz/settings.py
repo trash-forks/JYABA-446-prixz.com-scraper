b74b44e94bfc8434f69f137e6807272d9f9b9d05
nepal <nepal@nepali.com>
2022-07-28 23:23:56 +0545

    added the file

--- prixz/settings.py
+++ prixz/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'prixz.spiders'
 #USER_AGENT = 'prixz (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -86,3 +86,15 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+import os
+
+data_folder = 'data'
+
+# if does not exist, create it
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+
+    
+FEED_FORMAT = 'csv'
+FEED_URI = f'data/{BOT_NAME}.csv'
+FEED_EXPORT_ENCODING = "utf-8-sig"
--- prixz/spiders/prixz_spider.py
+++ prixz/spiders/prixz_spider.py
@@ -1,10 +1,110 @@
+from pyexpat import model
 import scrapy
+import datetime
 
 
-class PrixzSpider(scrapy.Spider):
+'''
+Date == Script run date  (DD/MM/YYYY) 
+
+Canal == “Prixz” 
+
+Category == category 
+
+Subcategory = Subcategory 
+
+Subcategory2= Subcategory2 
+
+Subcategory3=  BLANK 
+
+Marca == Brand 
+
+Modelo == Model 
+
+SKU ==SKU 
+
+UPC == UPC 
+
+Item == Item 
+
+Item Characteristics == Item Characteristics 
+
+URL SKU == URL 
+
+Image == image 
+
+Price == Price 
+
+Sale Price == Sale Price 
+
+Shipment Cost == BLANK 
+
+Sales Flag == Sales Flag 
+
+Store ID == BLANK 
+
+Store Name = BLANK 
+
+Store Address = BLANK 
+
+Stock == Stock 
+
+UPC WM == UPC[0:-1].zifll(16) 
+
+Final Price == min (price, sale price). 
+'''
+class PrixzSpider(scrapy.spiders.SitemapSpider):
     name = 'prixz'
-    allowed_domains = ['prixz.com']
-    start_urls = ['https://prixz.com/']
-
+    sitemap_urls = ['https://prixz.com/sitemap_index.xml']
+    sitemap_rule = [
+        ('/c/', 'parse'),
+    ]
     def parse(self, response):
-        pass
+        title = response.xpath("//h1[contains(@class,'product_title')]/text()").extract_first()
+        stock = response.xpath("//div[contains(@class,'stock_disponible')]/b/text()").extract_first()
+        price = response.xpath("//span[contains(@class,'woocommerce-Price-amount')]/bdi/text()").extract_first()
+        sale_price =  response.xpath("//ins/span[contains(@class,'woocommerce-Price-amount')]/bdi/text()").extract_first()
+        shipment_cost = response.xpath("//svg[contains(@class,'ui-pdp-icon--truck')]/parent::figure/following-sibling::div/p/text()").extract_first()
+        sku = response.xpath('//span[@class="sku"]/text()').extract_first()
+        presenticion = response.xpath('//p[contains(text(),"Presentación")]/parent::td/following-sibling::td/p/text()').extract_first()
+        image_url =  response.xpath("//a[contains(@class,'woocommerce-main-image')]/@href").extract_first()
+        description = response.xpath("//div[@id='description']//text()").extract()
+        clean_description = " ".join([d.strip() for d in description if d.strip()])
+        characteristics = response.xpath("//div[@id='product-info-table']//text()").extract()
+        clean_characteristics = " ".join([c.strip() for c in characteristics if c.strip()])
+        breadcum_list = response.xpath("//nav/a/text()").extract()[1:-1]
+        categories_dict = {breadcum_list.index(c):c for c in breadcum_list}
+        item = dict()
+        item['Date'] = datetime.datetime.now().strftime("%d/%m/%Y")
+        item['Canal'] = 'Prixz'
+        item['Category'] = categories_dict.get(0, '')
+        item['Subcategory'] = categories_dict.get(1, '')
+        item['Subcategory2'] = categories_dict.get(2, '')
+        item['Subcategory3'] = ''
+        item['Marca'] = breadcum_list[-1]
+        item['Modelo'] = presenticion
+        item['SKU'] = response.xpath("//input[contains(@name,'_sku')]/@value").extract_first()
+        item['UPC'] = sku
+        item['Item'] = title
+        item['Item Characteristics'] = f'{clean_description} {clean_characteristics}'
+        item['URL SKU'] = response.url
+        item['Image'] = image_url
+        item['Price'] = price
+        item['Sale Price'] = sale_price
+        item['Shipment Cost'] = shipment_cost
+        item['Sales Flag'] = ''
+        item['Store ID'] = ''
+        item['Store Name'] = ''
+        item['Store Address'] = ''
+        item['Stock'] = stock
+        item['UPC WM'] = sku[0:-1].zfill(16)
+        item['Final Price'] = min(price, sale_price) if price and sale_price else price
+        yield item
+
+
+
+
+
+
+
+    
+        
